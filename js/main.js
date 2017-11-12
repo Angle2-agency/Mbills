f0494210fc97e55f9ffd6e8c5b01518f4e488c18
Scooter)Ok <scooterok@gmail.com>
2017-11-12 23:53:38 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+$('#nav-button').click(function(){
+	if($('.nav__left').is(':hidden')){		
+		$('.nav__left').fadeIn(300, function(){
+			$('.nav__left aside').addClass('show-nav');
+		});
+		$('.nav__butto_icon').addClass('show-nav');		
+	}else{
+		$('.nav__left aside').removeClass('show-nav');
+		$('.nav__butto_icon').removeClass('show-nav');
+		setTimeout(function(){
+			$('.nav__left').fadeOut(300);	
+		}, 200);
+		
+	}
+	$('body').toggleClass('show-nav');
+})
