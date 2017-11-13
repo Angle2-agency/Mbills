4bcab239affa697f84464898d5bba43d80604eac
Scooter)Ok <scooterok@gmail.com>
2017-11-13 17:29:34 +0200

    frame 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+var myScroll;
 $('.nav__left').click(function(e) {
 	if(!$(e.target).closest('aside.show-nav').length)$('#nav-button').click();
 	console.log(s);
@@ -8,6 +8,16 @@ $('.nav__left').click(function(e) {
 $('#nav-button').click(function(){
 	if($('.nav__left').is(':hidden')){		
 		$('.nav__left').fadeIn(300, function(){
+			console.log($('.nav__left aside').height());
+			if($('.nav__left aside').height() < 900){
+				$('.nav__left_footer').addClass('na');
+				myScroll = myScroll || new IScroll('.nav__left aside', {
+				    mouseWheel: true,
+				    scrollbars: true
+				});
+			}else{
+				$('.nav__left_footer').removeClass('na');
+			}
 			$('.nav__left aside').addClass('show-nav');
 		});
 		$('.nav__butto_icon').addClass('show-nav');		
